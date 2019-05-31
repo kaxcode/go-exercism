ba76aa1c0762c5cc0d13fc99be68e5ac697ca6e7
Kenia <keniavalladarez@gmail.com>
2019-05-31 09:06:31 -0400

    Refactoring code.

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,22 +1,24 @@
 package hamming
 
 import "errors"
+
 // Distance returns a count of differences between two homologous DNA strands
 func Distance(a, b string) (int, error) {
 	count := 0
 
-	if len(a) == len(b) {
-		for i := 0; i < len(a); i++ {
-			if a[i] == b[i] {
-				continue
-			}
-			if a[i] != b[i] {
-				count++
-			}
-		}
-
-		return count, nil
+	if len(a) != len(b) {
+		return count, errors.New("Sequences are not of equal length")
 	}
 
-	return count, errors.New("Sequences are not of equal length")
+	for i := range a {
+		if a[i] == b[i] {
+			continue
+		}
+		if a[i] != b[i] {
+			count++
+		}
+	}
+
+	return count, nil
+
 }
