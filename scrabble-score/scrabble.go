e386e60e7587d2d880dc1dd327a95f8ccf26a9e7
Kenia <keniavalladarez@gmail.com>
2019-06-11 11:18:50 -0400

    Solution for scrabble.

--- scrabble-score/scrabble.go
+++ scrabble-score/scrabble.go
@@ -2,50 +2,30 @@ package scrabble
 
 import (
 	"strings"
-	"fmt"
 )
 
 // Score computes the scrabble score for the word given
 func Score(word string) int {
 	total := 0
 	w := strings.ToUpper(word)
-	wordSplit := strings.Split(w, "")
 
-	for _, letter := range wordSplit {
-		count := 0
+	for _, letter := range w {
 		l := string(letter)
-		fmt.Println(l)
-
 		if strings.ContainsAny(l, "A E I O U L N R S T") {
-			count++
+			total++
+		} else if strings.ContainsAny(l, "D G") {
+			total += 2
+		} else if strings.ContainsAny(l, "B C M P") {
+			total += 3
+		} else if strings.ContainsAny(l, "F H V W Y") {
+			total += 4
+		} else if strings.ContainsAny(l, "K") {
+			total += 5
+		} else if strings.ContainsAny(l, "J X") {
+			total += 8
+		} else if strings.ContainsAny(l, "Q Z") {
+			total += 10
 		}
-
-		if strings.ContainsAny(l, "D G") {
-			return count + 2
-		}
-
-		if strings.ContainsAny(l, "B C M P") {
-			return count + 3
-		}
-
-		if strings.ContainsAny(l, "F H V W Y") {
-			return count + 4
-		}
-
-		if strings.ContainsAny(l, "K") {
-			return count + 5
-		}
-
-		if strings.ContainsAny(l, "J X") {
-			return count + 8
-		}
-
-		if strings.ContainsAny(l, "Q Z") {
-			return count + 10
-		}
-
-		fmt.Println(count)
-		return count
 	}
 
 	return total
