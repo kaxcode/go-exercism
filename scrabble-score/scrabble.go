165ce2d8d2bacde04f9d796de4a8b87efcb40d44
Kenia <keniavalladarez@gmail.com>
2019-06-14 11:01:50 -0400

    Refactor

--- scrabble-score/scrabble.go
+++ scrabble-score/scrabble.go
@@ -1,29 +1,31 @@
+// Package scrabble returns scrabble score totals.
 package scrabble
 
 import (
 	"strings"
 )
 
-// Score computes the scrabble score for the word given
+// Score computes the scrabble score for the word given.ß
 func Score(word string) int {
 	total := 0
 	w := strings.ToUpper(word)
 
 	for _, letter := range w {
 		l := string(letter)
-		if strings.ContainsAny(l, "A E I O U L N R S T") {
+		switch {
+		case strings.ContainsAny(l, "A E I O U L N R S T"):
 			total++
-		} else if strings.ContainsAny(l, "D G") {
+		case strings.ContainsAny(l, "D G"):
 			total += 2
-		} else if strings.ContainsAny(l, "B C M P") {
+		case strings.ContainsAny(l, "B C M P"):
 			total += 3
-		} else if strings.ContainsAny(l, "F H V W Y") {
+		case strings.ContainsAny(l, "F H V W Y"):
 			total += 4
-		} else if strings.ContainsAny(l, "K") {
+		case strings.ContainsAny(l, "K"):
 			total += 5
-		} else if strings.ContainsAny(l, "J X") {
+		case strings.ContainsAny(l, "J X"):
 			total += 8
-		} else if strings.ContainsAny(l, "Q Z") {
+		case strings.ContainsAny(l, "Q Z"):
 			total += 10
 		}
 	}
